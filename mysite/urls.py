5813e8b6ba02e4fda1470539f8736bfb910d95bd
ylu021 <yichen_0@hotmail.com>
2017-01-28 16:27:02 -0500

    legacy views using generic django views

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views import generic
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',
+        generic.TemplateView.as_view(template_name='view1.html') #im giving a view1.html as the default page for home
+    )
 ]
