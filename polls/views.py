3a1e13987c7903311025211e277a364585bf61ed
ylu021 <yichen_0@hotmail.com>
2017-01-29 17:32:51 -0500

    adding detail views

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
-from django.http import HttpResponse
-
-from django.template import loader #load template
+from django.http import HttpResponse, Http404
+#from django.template import loader #load template
 from .models import Question #use the model
 
 #Create your views here.
 
 def index(request):
 	q_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
+	#template = loader.get_template('polls/index.html')
 	context = {
 		'latest_question_list': q_list,
 	}
  
 	#return HttpResponse("Hello, world. You're at the polls index.")
-	return HttpResponse(template.render(context, request)) #render model on template
+	return render(request, 'polls/index.html', context) #render model on template
 
 def detail(request, question_id):
-	return HttpResponse("You are look at question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	#return HttpResponse("You are look at question %s." % question_id)
+	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
 	return HttpResponse("You are looking at the results of question %s." % question_id)
