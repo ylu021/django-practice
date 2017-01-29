c308ec7a85573e7e3fbe83c8dcbe56875e9ca37a
ylu021 <yichen_0@hotmail.com>
2017-01-29 15:26:57 -0500

    setting aware of statics

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mysite/static'), #project's static folder 
+    os.path.join(BASE_DIR, 'mysite/static'), #project's static folder
 ]
 
 INSTALLED_APPS = [
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
