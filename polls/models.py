2207663bd9f0895029e5ebd653819129d30aa917
ylu021 <yichen_0@hotmail.com>
2017-01-29 16:52:15 -0500

    adding superuser and adding Question model to admin

--- polls/models.py
+++ polls/models.py
@@ -1,16 +1,23 @@
 from __future__ import unicode_literals
 
+import datetime
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200) #like sql setting vartype
 	pub_date = models.DateTimeField('date published')
+	def __str__(self):
+		return self.question_text
 
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1) #example
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE) #similar to sql
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+	def __str__(self):
+                return self.choice_text #print out what is inside the object
 
 
