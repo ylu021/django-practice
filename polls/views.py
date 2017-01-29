0e27a4b7529605402aa76e6b7b117ecc6228cc0d
ylu021 <yichen_0@hotmail.com>
2017-01-29 17:17:16 -0500

    adding template

--- polls/views.py
+++ polls/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.template import loader #load template
+from .models import Question #use the model
+
 #Create your views here.
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	q_list = Question.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_question_list': q_list,
+	}
+ 
+	#return HttpResponse("Hello, world. You're at the polls index.")
+	return HttpResponse(template.render(context, request)) #render model on template
 
 def detail(request, question_id):
 	return HttpResponse("You are look at question %s." % question_id)
